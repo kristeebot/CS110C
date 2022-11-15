9be67adfc9b3ff6244631ddb4a0ad1af4e848a2d
kristeebot <kristinahelwing@Kristinas-iMac.attlocal.net>
2022-11-14 18:36:07 -0800

    committing everything

--- Module11/Assignment11.cpp
+++ Module11/Assignment11.cpp
@@ -16,11 +16,11 @@ You'll also need to adjust the sample main() above to work with objects of your 
 */
 class Patient
 {
-public:           // Access specifier
-    string name;  // Attribute
-    int priority; // Attribute
+public:
+    string name;
+    int priority;
     Patient(string x, int y)
-    { // Constructor with parameters
+    {
         name = x;
         priority = y;
     }
@@ -33,29 +33,121 @@ bool operator<(const Patient &p1, const Patient &p2)
 {
     return p1.priority < p2.priority;
 }
+bool operator>(const Patient &p1, const Patient &p2)
+{
+    return p1.priority > p2.priority;
+}
 bool operator<=(const Patient &p1, const Patient &p2)
 {
     return p1.priority <= p2.priority;
 }
+
 template <class ItemType>
-
-// heap is going to need an array (or vector) of items
-
 class Heap
 {
 private:
     vector<ItemType> items;
     void heapify();
+    void heapifyDown(int rootIndex);
+    bool isLeaf(int index);
+    int getLeftIndex(int index)
+    {
+        return index * 2 + 1;
+    }
+    int getRightIndex(int index)
+    {
+        return index * 2 + 2;
+    }
 
 public:
     bool add(ItemType item);
     bool remove();
     ItemType peekTop();
+    void print();
+    bool isEmpty()
+    {
+        return items.empty();
+    }
 };
+
+template <class ItemType>
+void Heap<ItemType>::print()
+{
+    for (int i = 0; i < items.size(); i++)
+    {
+        ItemType item = items[i];
+        std::cout << item << endl;
+    }
+}
+
+template <class ItemType>
+bool Heap<ItemType>::isLeaf(int index)
+{
+    int leftIndex = getLeftIndex(index);
+    if (leftIndex >= items.size())
+    {
+        // If the left index exceeds our total,
+        // then we know there's no right index,
+        // and thus we're a leaf node.
+        return true;
+    }
+    return false;
+}
+
+template <class ItemType>
+void Heap<ItemType>::heapifyDown(int rootIndex)
+{
+    if (isLeaf(rootIndex))
+    {
+        return;
+    }
+    // The root must have a left child; assume it is the larger child
+    int largerChildIndex = getLeftIndex(rootIndex);
+    int rightIndex = getRightIndex(rootIndex);
+
+    // now see if there is a right child which is larger than left child
+    if (rightIndex < items.size())
+    {
+        // There's a right child, but is the right child bigger?
+        if (items[rightIndex] > items[largerChildIndex])
+        {
+            largerChildIndex = rightIndex;
+        }
+    }
+    // If item in root is smaller than item in larger child, swap items
+    if (items[rootIndex] < items[largerChildIndex])
+    {
+        iter_swap(items.begin() + rootIndex, items.begin() + largerChildIndex);
+    }
+
+    // Transform the semiheap rooted at largerChildIndex into a heap
+    heapifyDown(largerChildIndex);
+
+    //     heapRebuild (rootIndex: integer, items: ArrayType, itemCount:integer)
+    // if (the root is not a leaf)
+    // {
+    // // The root must have a left child; assume it is the larger child
+    // largerChildIndex = 2 * rootIndex + 1 // Left child index
+    // // now see if there is a right child which is larger than left child
+    // if (the root has a right child)
+    // {
+    // rightChildIndex = 2 * rootIndex + 2 // Right child index
+    // if (items[rightChildIndex] > items[largerChildIndex])
+    // largerChildIndex = rightChildIndex // Larger child index
+    // }
+    // // If item in root is smaller than item in larger child, swap items
+    // if (items[rootIndex] < items[largerChildIndex])
+    // {
+    // Swap items[rootIndex] and items[largerChildIndex]
+    // // Transform the semiheap rooted at largerChildIndex into a heap
+    // heapRebuild (largerChildIndex, items, itemCount)
+    // }
+    // }
+    // else root is a leaf, so we are done
+}
 template <class ItemType>
 void Heap<ItemType>::heapify()
 {
-    // trickle up swapping all the way to the root, if necessary
     //  trickle new item up to the right spot in the tree
     int newDataIndex = items.size() - 1;
     if (newDataIndex == 0)
@@ -72,7 +164,7 @@ void Heap<ItemType>::heapify()
         }
         else
         {
-            std::iter_swap(items.begin()+newDataIndex,items.begin()+parentIndex);
+            std::iter_swap(items.begin() + newDataIndex, items.begin() + parentIndex);
             newDataIndex = parentIndex;
         }
     }
@@ -88,6 +180,10 @@ bool Heap<ItemType>::add(ItemType item)
 template <class ItemType>
 bool Heap<ItemType>::remove()
 {
+    ItemType item = items.back();
+    items.pop_back();
+    items[0] = item;
+    heapifyDown(0);
     return true;
 }
 template <class ItemType>
@@ -109,8 +205,6 @@ int main()
     Patient patientObj8("Jennifer", 6);
     Patient patientObj9("Chang", 5);
 
-    cout << patientObj1 << endl;
-
     Heap<Patient> ER;
     ER.add(patientObj1);
     ER.add(patientObj2);
@@ -129,5 +223,11 @@ int main()
     ER.remove();
     cout << ER.peekTop() << " will now see the doctor.\n";
     ER.remove();
+    ER.print();
+    while (!ER.isEmpty())
+    {
+        cout << ER.peekTop() << " will now see the doctor.\n";
+        ER.remove();
+    }
     return 0;
 }
